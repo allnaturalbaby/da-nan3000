455151fcfc09a0f9f9b239dde3b6d2110909baa1
Hevos <hevos93@gmail.com>
2022-02-13 12:42:51 -0800

    Added port 80 and root seperation

---
+++ README.md
@@ -0,0 +1,4 @@
+# MP1
+improved.c is the new working file.
+
+UID is set to 1000, your local user.
--- improved.c
+++ improved.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
 
 #define LOKAL_PORT 80
 #define BAK_LOGG 10 // Størrelse på for kø ventende forespørsler
@@ -82,8 +86,61 @@ void handleHttpRequest(int sd)
   }
 }
 
-int main()
-{
+static void skelly_daemon(){
+    pid_t pid;
+    pid = fork();
+    
+    if (pid < 0){
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid > 0){
+        exit(EXIT_SUCCESS);
+    }
+
+    if (setsid() < 0){
+        exit(EXIT_FAILURE);
+    }
+
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    pid = fork();
+
+    if (pid < 0){
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid > 0){
+        exit(EXIT_SUCCESS);
+    }
+
+    umask(0); //fakking up?
+
+    chroot("var/www/mp1");
+
+    int x;
+    for (x=sysconf(_SC_OPEN_MAX); x>=0; x--){
+        close(x);
+    }
+}
+
+int privilege(){
+
+    uid_t uid = 1000;
+    gid_t gid = 1000;
+
+    if (getuid() == 0) {
+        setuid(uid);
+        setgid(gid);
+    }
+
+    if (setuid(0) != -1){
+        exit(0);
+    }
+}
+
+int web_service(){
   struct sockaddr_in lok_adr;
   int sd, ny_sd;
 
@@ -104,6 +161,8 @@ int main()
   else
     exit(1);
 
+  privilege();//root drit
+
   // Venter på forespørsel om forbindelse
   listen(sd, BAK_LOGG);
   while (1)
@@ -116,13 +175,6 @@ int main()
     {
 
       dup2(ny_sd, 1); // redirigerer socket til standard utgang
-
-      /*printf("HTTP/1.1 200 OK\n");
-      printf("Content-Type: text/plain\n");
-      printf("\n");
-      printf("Hallo klient!\n");*/
-
-      //readAsis("test.asis");
 
       handleHttpRequest(ny_sd);
 
@@ -140,4 +192,12 @@ int main()
     }
   }
   return 0;
+}
+
+int main(){
+    skelly_daemon();
+
+    while(1){
+        web_service();
+    }
 }
