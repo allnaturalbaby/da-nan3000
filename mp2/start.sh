1e6c86e7e707efa4bf24ab3c67eb3c4598907801
Hevos93 <Hevos93@gmail.com>
2022-03-14 12:58:43 +0100

    new start mp2

--- mp2/start.sh
+++ mp2/start.sh
@@ -1,6 +1,12 @@
-./scripts/kill.sh
-sudo ./scripts/create_log.sh
-gcc improved.c -o improved
-sudo ./improved
+cd testenv
+rm -r container
+
+AUXPID=$(ps aux | less | grep ./improved | head -1 | awk '{ print $2 }')
+kill -9 $AUXPID
+
+JOBPID=$(jobs -p | awk '{ print $3 }')   
+kill -9 $JOBPID
+
+./container-init.sh
 
 echo "Have a nice day /redacted/ ;]"
