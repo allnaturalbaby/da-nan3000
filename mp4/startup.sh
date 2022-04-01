53aca96f56b211429491adff7ca422ea0c9a68b1
chrtho13 <christiandenandre@hotmail.com>
2022-04-01 19:00:02 +0200

    mp4 cgi working on container

--- mp4/startup.sh
+++ mp4/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t shelltestserver .
-docker run -dit --userns=host --name Kontainer-3 -p 8080:80 shelltestserver 
-docker run --userns=host -dit --name Kontainer-2 -p 8180:80 shelltestserver
+docker run -dit --name Kontainer-3 -p 8080:80 shelltestserver 
+docker run -dit --name Kontainer-2 -p 8180:80 shelltestserver
